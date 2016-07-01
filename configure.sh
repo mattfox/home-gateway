8aa715c6eba254ab208e7fc97bfeb91d89377137
Matt Fox <matt@tansen.ca>
2016-06-30 20:41:09 -0700

    Add configure.sh

---
+++ configure.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Configure the system for a basic salt install, such as salt install via easy_install.
+set -e
+
+mkdir --parent /etc/salt/
+mkdir --parent /var/log/salt/minion
+mkdir --parent /var/cache/salt/minion/
+
+echo 'file_client: local' > /etc/salt/minion
+cp /etc/hostname /etc/salt/minion_id
